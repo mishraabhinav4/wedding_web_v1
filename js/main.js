a02aa14a4c0214ff06b9d10017cf453db797cdc6
Abhinav Mishra <mishra.abhinav4@gmail.com>
2024-10-13 13:47:14 +0200

    added timer

--- js/main.js
+++ js/main.js
@@ -281,7 +281,7 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-  let birthday = "Dec 15, 2023 00:00:00",
+  let birthday = "Dec 10, 2024 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
         let now = new Date().getTime(),
